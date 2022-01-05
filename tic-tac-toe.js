bb387dd401c8b1769212737c1e89adb171260d13
Mihai Austrianu <mihaiaustrianu@gmail.com>
2022-01-05 23:49:39 +0200

    fixed minor bugs

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,37 +5,37 @@ const gameBoard = (() => {
   function updateElement(id, playerSymbol) {
     board[id] = playerSymbol;
   }
-  function clearBoard(){
-    for ( let i=0 ; i< 9;i++ ){
-      board[i]=''
+  function clearBoard() {
+    for (let i = 0; i < 9; i++) {
+      board[i] = "";
     }
   }
   return {
     board,
     updateElement,
-    clearBoard
+    clearBoard,
   };
 })();
 
 //Display Controller module
 const displayController = (() => {
-
   const statusDisplay = document.querySelector(".game-status");
-  const winningMessage = () => `Player ${gameController.getPlayer()}  won!`;
+  
   const drawMessage = () => `Game ended in a draw!`;
   const currentPlayerTurn = () =>
-    `It's player ${gameController.getPlayer()}'s turn`;
+    `It's player ${switchP()}'s turn`;
   const button = document.querySelector(".restart");
 
-  function updateDisplay(roundWon, roundDraw, gameActive) {
-    console.log(gameBoard.board);
+  function updateDisplay(roundWon, roundDraw) {
+    console.log('display');
+    console.log(gameController.getPlayer());
+    let winningMessage = () => `Player ${gameController.getPlayer()}  won!`;
     for (let item in gameBoard.board) {
       let element = document.getElementById(`position-${item}`);
       element.innerHTML = gameBoard.board[item];
     }
     if (roundDraw == false && roundWon == false) {
-      button.style.display = "none";
-      statusDisplay.innerHTML = currentPlayerTurn();
+      displayStatus();
     } else if (roundWon == true) {
       statusDisplay.innerHTML = winningMessage();
       button.style.display = "inline-block";
@@ -44,11 +44,23 @@ const displayController = (() => {
       button.style.display = "inline-block";
     }
   }
+  function switchP(){
+    if(gameController.getPlayer() ==1)
+    return 2;
+    else
+    return 1;
+  }
 
+  function displayStatus() {
+    button.style.display = "none";
+    statusDisplay.innerHTML = currentPlayerTurn();
+  }
   function resetHandler() {
     gameBoard.clearBoard();
+    displayStatus();
     updateDisplay();
-    gameController.setGameActive()
+    gameController.setP()
+    gameController.setGameActive();
   }
 
   return {
@@ -56,9 +68,10 @@ const displayController = (() => {
     resetHandler: resetHandler,
   };
 })();
+
 //Game controller
 const gameController = (() => {
-  playerTurnValue = 1;
+  let playerTurnValue = 1;
   gameActive = true;
 
   const winningConditions = [
@@ -71,11 +84,15 @@ const gameController = (() => {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
+  function setP(){
+    playerTurnValue =1 ;
+  }
   function getPlayer() {
     return playerTurnValue;
   }
 
-  function setGameActive(){
+  function setGameActive() {
     gameActive = true;
   }
 
@@ -85,26 +102,26 @@ const gameController = (() => {
     } else playerTurnValue = 1;
   }
   function playerMove(id) {
-      let playerSymbol;
-      if (playerTurnValue == 1) {
-        playerSymbol = "X";
-      } else playerSymbol = "0";
-      gameBoard.updateElement(id, playerSymbol);
-      playerTurn();
-    
+    let playerSymbol;
+    if (playerTurnValue == 1) {
+      playerSymbol = "X";
+    } else playerSymbol = "0";
+    gameBoard.updateElement(id, playerSymbol);
   }
   function handleCellClick(clickedCellEvent) {
     const clickedCell = clickedCellEvent.target;
     const clickedCellIndex = parseInt(
       clickedCell.getAttribute("data-item-index")
     );
-    console.log(clickedCellIndex);
+    //console.log(clickedCellIndex);
     if (gameBoard.board[clickedCellIndex] !== "" || !gameActive) {
       return;
     }
     playerMove(clickedCellIndex, playerTurnValue);
+    
     [roundWon, roundDraw] = handleResult();
-    displayController.updateDisplay(roundWon, roundDraw);
+    displayController.updateDisplay(roundWon, roundDraw,playerTurn);
+    playerTurn();
   }
   function handleResult() {
     let roundWon = false;
@@ -118,22 +135,22 @@ const gameController = (() => {
       }
       if (a === b && b === c) {
         roundWon = true;
-        playerTurnValue = 1;
         gameActive = false;
         break;
       }
     }
     let roundDraw = !gameBoard.board.includes("");
     if (roundDraw == true) {
-      playerTurnValue = 1;
       gameActive = false;
     }
     return [roundWon, roundDraw];
   }
+
   return {
     handleCellClick,
     getPlayer,
-    setGameActive
+    setGameActive,
+    setP
   };
 })();
 
